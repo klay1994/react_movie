64088fb5dc22d93c8fae9e9e8787e5b2b2b6afcb
klay1994 <913013850@qq.com>
2019-10-31 01:35:38 +0800

    Updates

---
+++ precache-manifest.c21c0cccf1c80e4659d5688f4ae90434.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8a005b7eb2453bafe30ec4cba3d3ee",
+    "url": "/react_movie/index.html"
+  },
+  {
+    "revision": "f8022c712b79700bf378",
+    "url": "/react_movie/static/css/2.b601d0af.chunk.css"
+  },
+  {
+    "revision": "f8022c712b79700bf378",
+    "url": "/react_movie/static/js/2.db5db772.chunk.js"
+  },
+  {
+    "revision": "498ddaa61dc3effb32b1",
+    "url": "/react_movie/static/js/main.c65eb21d.chunk.js"
+  },
+  {
+    "revision": "f1fc312c342be87fac2f",
+    "url": "/react_movie/static/js/runtime-main.96387ee0.js"
+  }
+]);
